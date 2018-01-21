0111641f491940e1891fa916ed1406d8d5eef7bf
Natchapon Saejueng <gui_desan@outlook.com>
2018-01-21 18:53:31 +0700

    Add necessary field and method

--- src/game/unit/Unit.java
+++ src/game/unit/Unit.java
@@ -1,5 +1,37 @@
 package game.unit;
 
 abstract class Unit {
+	
+	protected String name;
+	protected Role role;
+	protected Side side;
+	protected Status status;
 
+	public String getName() {
+		return name;
+	}
+
+	public Role getRole() {
+		return role;
+	}
+
+	public Side getSide() {
+		return side;
+	}
+
+	public Status getStatus() {
+		return status;
+	}
+
+	protected enum Side {
+		VILLAGER, WEREWOLF
+	}
+	
+	protected enum Role {
+		VILLAGER, WEREWOLF, SEER
+	}
+	
+	protected enum Status {
+		ALIVE, DEAD
+	}
 }
