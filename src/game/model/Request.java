6997d69c1a54f9918767d676fad9e18e659ce8ce
Natchapon Saejueng <gui_desan@outlook.com>
2018-01-23 00:34:53 +0700

    Add method in Server.java
    Method added:
    - getRequestType
    - getParams

--- src/game/model/Request.java
+++ src/game/model/Request.java
@@ -6,10 +6,19 @@ public class Request implements Serializable {
 
 	private static final long serialVersionUID = -4622734590525123953L;
 	private RequestType requestType;
-	private Object args;
+	private Object[] params;
 	
-	public Request(RequestType requestType, Object... args) {
-		
+	public Request(RequestType requestType, Object[] params) {
+		this.requestType = requestType;
+		this.params = params;
+	}
+
+	public RequestType getRequestType() {
+		return requestType;
+	}
+
+	public Object getParams(int index) {
+		return params[index];
 	}
 
 	public enum RequestType {
