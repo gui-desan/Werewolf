803ac95fae1d076c4b6e13bba180f30f9d54c751
Natchapon Saejueng <gui_desan@outlook.com>
2018-01-23 00:25:13 +0700

    Add RequestType enum in Request.java

--- src/game/model/Request.java
+++ src/game/model/Request.java
@@ -1,5 +1,18 @@
 package game.model;
 
-public class Request {
+import java.io.Serializable;
 
+public class Request implements Serializable {
+
+	private static final long serialVersionUID = -4622734590525123953L;
+	private RequestType requestType;
+	private Object args;
+	
+	public Request(RequestType requestType, Object... args) {
+		
+	}
+
+	public enum RequestType {
+		
+	}
 }
